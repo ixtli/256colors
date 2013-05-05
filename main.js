9ad97717dd25b9db2d18d5c5996a8f196d483541
Chris Galardi <ixplode@gmail.com>
2013-05-05 12:23:53 -0400

    it basically computes everything

--- main.js
+++ main.js
@@ -2,29 +2,75 @@
 function Picker ()
 {
 	this.pickerContainer = null;
-	this.chipClickHandler = null;
+	this.shortcutsContainer = null;
+	this.inputElement = null;
 	
-	this.bgEscapePrefix = '\\033[48;5;';
-	this.fgEscapePrefix = '\\003[38;5;';
+	this.chipClickHandler = null;
+	this.shortcutClickHandler = null;
+	
+	this.escapeCharacter = '\\033';
+	
+	this.bgEscapePrefix = '[48;5;';
+	this.fgEscapePrefix = '[38;5;';
 	this.escapeSuffix = 'm';
+	
+	this.shortcuts = {
+		'clear': '[0'
+	};
 };
 
 Picker.prototype.init = function ()
 {
+	// Collect DOM elements necessary to construct the application
 	this.pickerContainer = $('div#picker');
+	this.inputElement = $('div#ps1 > div#input');
+	this.shortcutsContainer = $('div#shortcuts');
 	
 	// Make proxies
 	this.chipClickHandler = $.proxy(this._chipClick, this);
+	this.shortcutClickHandler = $.proxy(this._shortcutClick, this);
 	
+	// Generate the UI
 	this._createPicker();
+	this._createShortcuts();
 	
 	return true;
 };
 
 /** @private */
+Picker.prototype._appendToPS1 = function (code)
+{
+	var toAppend = this.escapeCharacter + code + this.escapeSuffix;
+	this.inputElement.html(this.inputElement.html() + toAppend);
+};
+
+/** @private */
+Picker.prototype._shortcutClick = function (evt)
+{
+	this._appendToPS1($(evt.currentTarget).attr('code'));
+};
+
+/** @private */
 Picker.prototype._chipClick = function (evt)
 {
-	
+	this._appendToPS1(this.bgEscapePrefix + $(evt.currentTarget).attr('code'));
+};
+
+/** @private */
+Picker.prototype._createShortcuts = function ()
+{
+	var container = this.shortcutsContainer;
+	var elt = null;
+	var scs = this.shortcuts;
+	for (cut in scs)
+	{
+		elt = $('<div>')
+			.addClass('shortcut')
+			.text(cut)
+			.attr('code', scs[cut])
+			.on('click', this.shortcutClickHandler);
+		container.append(elt);
+	}
 };
 
 /** @private */
@@ -57,4 +103,5 @@ Picker.prototype._createPicker = function ()
 	
 };
 
+// Bootstrap when DOM is ready
 $( function () { var p = new Picker(); p.init(); } );
